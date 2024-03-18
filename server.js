914a4eb460ae1b349b8310b2fde5e1afeeb6d70f
Cebastion <99135718+Cebastion@users.noreply.github.com>
2024-03-18 19:26:09 +0200

    new commit

--- server.js
+++ server.js
@@ -7,6 +7,22 @@ server.get('/', (req, res) => {
     res.sendFile('./index.html')
 })
 
+server.post('/login', (req, res) => {
+
+})
+
+server.post('/sign', (req, res) => {
+
+})
+
+server.post('/add_order', (req, res) => {
+
+})
+
+server.get('/history', (req, res) => {
+    
+})
+
 server.listen(HOST, () => {
     console.log('ready!')
 })
